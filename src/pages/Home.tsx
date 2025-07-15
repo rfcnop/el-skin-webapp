56001889bea7591a15e7fa21a27536c6f9c317ac
Ramon Francescolli Costa <rfcosta@mppr.mp.br>
2025-07-15 15:53:17 -0300

    Card de Produto

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import Carousel from '../componentes/Carousel';
+import ProductCard from '../componentes/ProductCard';
+import products from '../data/products.json';
 
 export default function Home() {
   return (
     <main>
       <Carousel />
+      <ProductCard {...products[0]} />
     </main>
   );
 }
---
+++ src/types/Product.tsx
@@ -0,0 +1,5 @@
+import products from '../data/products.json';
+
+type Product = typeof products[0];
+
+export default Product;
